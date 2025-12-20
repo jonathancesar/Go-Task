1bc3472271dd758a0006bf03f5ede245c6dbfa1b
jonathancesar <jonathancesar1995@gmail.com>
2025-12-20 14:23:36 -0300

    Componentes necessários e service em desenvolvimento

---
+++ src/app/components/services/modal-controller.service.ts
@@ -0,0 +1,34 @@
+import { inject, Injectable } from '@angular/core';
+import { Dialog } from '@angular/cdk/dialog';
+import { TaskFormModalComponent } from '../task-form-modal/task-form-modal.component';
+import { TaskCommentsModalComponent } from '../task-comments-modal/task-comments-modal.component';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ModalControllerService {
+  private readonly modalSizeOptions = {
+    maxWidth: '620px',
+    width: '95%',
+  };
+  private readonly _dialog = inject(Dialog);
+
+  openNewTaskModal() {
+    //Para nao repetir o código Altura e largura atribuir o modalSizeOptions
+    return this._dialog.open(TaskFormModalComponent, {
+      ...this.modalSizeOptions,
+    });
+  }
+
+  openEditTaskModal() {
+    return this._dialog.open(TaskFormModalComponent, {
+      ...this.modalSizeOptions,
+    });
+  }
+
+  openTaskCommentsModal() {
+    return this._dialog.open(TaskCommentsModalComponent, {
+      ...this.modalSizeOptions,
+    });
+  }
+}
