b2bbc8d9b850dbb862b086f40bce9dabf853121f
jonathancesar <jonathancesar1995@gmail.com>
2026-01-06 20:25:08 -0300

    Drag Drop e função service addTask

--- src/app/components/services/task.service.ts
+++ src/app/components/services/task.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, map } from 'rxjs';
 import { ITask } from '../../interfaces/task.interface';
+import { ITaskFormControls } from '../../interfaces/task-form-controls.interface';
+import { TaskStatusEnum } from '../../enums/task-status.enum';
+import { generateUniqueIdWithTimesTamp } from '../../utils/generate-unique-id-with-timestamp';
 
 @Injectable({
   providedIn: 'root',
@@ -8,13 +11,36 @@ import { ITask } from '../../interfaces/task.interface';
 export class TaskService {
   //Tarefas em a Fazer
   private todoTasks$ = new BehaviorSubject<ITask[]>([]);
-  readonly todoTasks = this.todoTasks$.asObservable();
+  readonly todoTasks = this.todoTasks$
+    .asObservable()
+    .pipe(map((tasks) => structuredClone(tasks)));
 
   //Tarefas em Fazendo
   private doingTasks$ = new BehaviorSubject<ITask[]>([]);
-  readonly doingTasks = this.doingTasks$.asObservable();
+  readonly doingTasks = this.doingTasks$
+    .asObservable()
+    .pipe(map((tasks) => structuredClone(tasks)));
 
   //Tarefas em Concluído
   private doneTasks$ = new BehaviorSubject<ITask[]>([]);
-  readonly doneTasks = this.doneTasks$.asObservable();
+  readonly doneTasks = this.doneTasks$
+    .asObservable()
+    .pipe(map((tasks) => structuredClone(tasks)));
+
+  addTask(taskInfos: ITaskFormControls) {
+    const newTask: ITask = {
+      ...taskInfos,
+      status: TaskStatusEnum.TODO,
+      id: generateUniqueIdWithTimesTamp(),
+      comments: [],
+    };
+
+    const currentList = this.todoTasks$.value;
+
+    this.todoTasks$.next([...currentList, newTask]);
+  }
+
+  carregarListaAtualDeTodos() {
+    console.log('Lista Atual TODOS', this.todoTasks$.value);
+  }
 }
